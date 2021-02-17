9085be4ce4a5bba4d26c687782ad954f7c2be120
ChaeEun Lee <taylor.lee1017@gmail.com>
2021-02-17 17:47:26 +0900

    Add files via upload

--- Greedy/Chap3_2_큰_수의_법칙.py
+++ Greedy/Chap3_2_큰_수의_법칙.py
@@ -12,18 +12,8 @@ first = data[0]
 second = data[1]
 
 # 합 계산
-result = 0        # 결과
-count = 0         # 숫자가 더해지는 횟수 카운트
-max_count = 0     # 연속 최대 횟수 카운트
-
-while count < m:
-    if max_count < k:
-        result = result + first
-        max_count += 1
-        count += 1
-    else:
-        result = result + second
-        max_count = 0
-        count += 1
+first_num = ((m // (k+1)) + (m % (k+1))) * k * first
+second_num = (m // (k+1)) * second
+result = first_num + second_num
 
 print(result)
